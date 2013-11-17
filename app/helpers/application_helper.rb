143eaa12a4f85e5ee270a5f063b6bb70d90334d7
Tee Parham <tee@neighborland.com>
2013-11-17 16:59:31 -0700

    add #rcache

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,13 @@ module ApplicationHelper
     return unless monkey
 		link_to "Search the Googs for #{monkey.name}", "http://google.com?q=#{ERB::Util.html_escape(monkey.name)}"
   end
-
+  
+  def rcache(name = {}, options = nil, &block)
+    if controller.perform_caching
+      safe_concat(fragment_for(name, options, &block))
+    else
+      yield
+    end
+    nil
+  end
 end
