ad43b9ee8985219d9373930798af95a5b3a81e32
Tee Parham <tee@neighborland.com>
2013-11-18 11:06:52 -0700

    use pure-css styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,12 @@ module ApplicationHelper
 
   def monkey_button(monkey)
     return unless monkey
-    link_to monkey.name, monkey_path(monkey), class: "random#{rand(2)}"
+    link_to monkey.name, monkey_path(monkey), class: 'pure-button pure-button-primary'
   end
 
   def googs_button(monkey)
     return unless monkey
-		link_to "Search the Googs for #{monkey.name}", "http://google.com?q=#{ERB::Util.html_escape(monkey.name)}"
+		link_to "Search for #{monkey.name}", "http://google.com?q=#{ERB::Util.html_escape(monkey.name)}", class: 'pure-button'
   end
   
   def rcache(name = {}, options = nil, &block)
