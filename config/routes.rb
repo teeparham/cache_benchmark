5a6546aca0fcf4660cc8733cf81b624ad9f77543
Tee Parham <tee@neighborland.com>
2013-11-17 17:28:10 -0700

    add RussianNoDigestController

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ CacheReplaceBenchmark::Application.routes.draw do
   resources :replace, only: [:index]
   resources :replace_collection, only: [:index]
   resources :russian, only: [:index]
+  resources :russian_nodigest, only: [:index]
   resources :monkeys, only: [:show]
 end
