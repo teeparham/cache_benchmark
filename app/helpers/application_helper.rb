34f0b8af6e223c8e7e825e069573676630878219
Tee Parham <tee@neighborland.com>
2013-11-18 15:50:18 -0700

    move replace hash to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
     return unless monkey
 		link_to "Search for #{monkey.name}", "http://google.com?q=#{ERB::Util.html_escape(monkey.name)}", class: 'pure-button'
   end
-  
+
   def rcache(name = {}, options = nil, &block)
     if controller.perform_caching
       safe_concat(fragment_for(name, options, &block))
@@ -19,4 +19,14 @@ module ApplicationHelper
     end
     nil
   end
+
+  def replace_monkeys_hash
+    {
+      monkey_button:        ->(monkey) { monkey_button(monkey) },
+      monkey_friend_button: ->(monkey) { monkey_button(monkey.best_friend) },
+      googs_buttons:        ->(monkey) { googs_button(monkey) + googs_button(monkey.best_friend) },
+    }
+
+  end
+
 end
