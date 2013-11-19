8d2f7a52cac3c0432879ad886be7fcf111d38dc4
Tee Parham <tee@neighborland.com>
2013-11-18 17:12:08 -0700

    add bash functions

--- bench.sh
+++ bench.sh
@@ -1,12 +1,38 @@
+ab_no_content() {
+  ab -r -n 15 http://127.0.0.1:3000/monkeys/1 | grep -m 1 " per request"
+}
+ab_no_cache() {
+  ab -r -n 15 http://127.0.0.1:3000/none | grep -m 1 " per request"
+}
+ab_russian() {
+  ab -r -n 15 http://127.0.0.1:3000/russian | grep -m 1 " per request"
+}
+ab_russian_no_digest() {
+  ab -r -n 15 http://127.0.0.1:3000/russian_nodigest | grep -m 1 " per request"
+}
+ab_rocket() {
+  ab -r -n 15 http://127.0.0.1:3000/replace | grep -m 1 " per request"
+}
+ab_rocket_collection() {
+  ab -r -n 15 http://127.0.0.1:3000/replace_collection | grep -m 1 " per request"
+}
+
+export -f ab_no_content
+export -f ab_no_cache
+export -f ab_russian
+export -f ab_russian_no_digest
+export -f ab_rocket
+export -f ab_rocket_collection
+
 echo 'No Content'
-ab -r -n 15 http://127.0.0.1:3000/monkeys/1 | grep ' per request'
+ab_no_content
 echo 'No Caching'
-ab -r -n 15 http://127.0.0.1:3000/none | grep ' per request'
+ab_no_cache
 echo 'Russian Doll'
-ab -r -n 15 http://127.0.0.1:3000/russian | grep ' per request'
+ab_russian
 echo 'Russian Doll - No Digests'
-ab -r -n 15 http://127.0.0.1:3000/russian_nodigest | grep ' per request'
+ab_russian_no_digest
 echo 'CacheRocket'
-ab -r -n 15 http://127.0.0.1:3000/replace | grep ' per request'
+ab_rocket
 echo 'CacheRocket - collection'
-ab -r -n 15 http://127.0.0.1:3000/replace_collection | grep ' per request'
+ab_rocket_collection
